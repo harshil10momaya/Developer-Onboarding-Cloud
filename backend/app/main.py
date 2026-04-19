55cab16403155a84acded53c4b4f76d86a07c592
Harshil <harshil10momaya@gmail.com>
2026-04-19 23:16:02 +0530

    final fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,11 +28,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        settings.FRONTEND_URL, 
-        "http://localhost", 
-        "http://127.0.0.1", 
-        "http://localhost:5173", 
-        "http://localhost:80"
+        "https://developer-onboarding-cloud-1.onrender.com",  # your frontend
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
